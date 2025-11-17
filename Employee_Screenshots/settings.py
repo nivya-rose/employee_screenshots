d5ae1f5ef7cf64a782d0e35e2dc59333448393d0
Shylekh325 <shylekh001@gmail.com>
2025-11-17 14:04:09 +0530

    STATIC_URL = '/staticfiles/admin/'

--- Employee_Screenshots/settings.py
+++ Employee_Screenshots/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 GOOGLE_DRIVE_FOLDER_ID = "13qefxLpR3ZitbyCHbKR73h9Wl4pcEov-"
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/staticfiles/admin/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
