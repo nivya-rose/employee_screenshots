80cb7d57292acbc5575225f89fc866a98c8a71d2
Shylekh325 <shylekh001@gmail.com>
2025-11-17 15:46:32 +0530

    Update settings.py

--- Employee_Screenshots/settings.py
+++ Employee_Screenshots/settings.py
@@ -139,7 +139,7 @@ GOOGLE_DRIVE_FOLDER_ID = "13qefxLpR3ZitbyCHbKR73h9Wl4pcEov-"
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / "staticfiles",]
+STATICFILES_DIRS = [BASE_DIR / "ScreenshotApp",]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressdManifestStaticFilesStorage"
 
