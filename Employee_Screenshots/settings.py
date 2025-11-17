6306123c44d6d6d41f46c53c19c7711885ba39eb
Shylekh325 <shylekh001@gmail.com>
2025-11-17 15:40:58 +0530

    Update settings.py

--- Employee_Screenshots/settings.py
+++ Employee_Screenshots/settings.py
@@ -141,8 +141,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "staticfiles",]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressdManifestStaticFilesStorage"
 
 LOGIN_URL = '/admin_login/'
